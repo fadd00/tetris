fbf93cbf2aa460bc995e50baaf633e582babd448
fadd00 <andhikahutama9@gmail.com>
2025-05-12 19:47:45 +0700

    commit

---
+++ src/app/tetris/page.tsx
@@ -0,0 +1,7 @@
+export default function Tetris() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Selamat Datang di Game Tetris!</h1>
+    </div>
+  );
+}
